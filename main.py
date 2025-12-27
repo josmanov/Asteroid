47f2d73c853055b5d8489e7c78664451caa5f578
josmanov <jusuf.osmanovic01@gmail.com>
2025-12-27 21:19:08 +0200

    create black screen window

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pygame
 import constants 
+from logger import log_state
+
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:", constants.SCREEN_WIDTH)
     print("Screen height:", constants.SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while (1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Exiting game...")
+                return
+        log_state()
+        screen.fill("black", None, 0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
